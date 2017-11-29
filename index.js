6e3233a343ef64e6369f50a8ce45a978f7c8bccb
hgcoder <651841877@qq.com>
2017-11-29 22:53:04 +0800

    1.0 hgcoder ci

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @authors hgcoder (you@example.org)
+ * @date    2017-11-29 22:42:57
+ * @version $Id$
+ */
+
---
+++ src/base.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-11-29 22:50:34
+ * @version $Id$
+ */
+
